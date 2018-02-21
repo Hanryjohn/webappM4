fb55106f09533121a143655da3a3581cfc57b44b
Mike-Van <miike.vaan@gmail.com>
2018-02-21 23:01:08 +0700

    added Login navigation bar

--- Project1/Controllers/AccountsController.cs
+++ Project1/Controllers/AccountsController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Project1.Models;
@@ -10,10 +12,19 @@ namespace Project1.Controllers
     public class AccountsController : Controller
     {
         // GET: Accounts
-        public ActionResult Index()
+        public ActionResult Index(string searchString)
         {
-            using (WebAppEntities db = new WebAppEntities())
-                return View(db.UserAccounts.ToList());         
+            ViewBag.Message = TempData["Message"];
+            ViewBag.Status = TempData["Status"];
+
+            WebAppEntities db = new WebAppEntities();
+            var users = from u in db.UserAccounts select u;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(a => a.UserName.Contains(searchString));
+            }
+            return View(users.ToList());         
         }
 
         public ActionResult Register()
@@ -96,7 +107,81 @@ namespace Project1.Controllers
                 //ViewBag.Message = "You have successfully logged out.";
                 //ViewBag.Status = "info";
                 return RedirectToAction("Login");
+            }            
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+                ;
+            UserAccount u = new WebAppEntities().UserAccounts.Find(id);
+            if (u == null)
+            {
+                return HttpNotFound();
+            }
+            return View(u);
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserAccount u = new WebAppEntities().UserAccounts.Find(id);
+            if (u == null)
+            {
+                return HttpNotFound();
+            }
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UserAccount acc)
+        {
+            if (ModelState.IsValid)
+            {
+                WebAppEntities db = new WebAppEntities();
+                db.Entry(acc).State = EntityState.Modified;
+                db.SaveChanges();                
+                ModelState.Clear();
+                TempData["Message"] = acc.UserName + " " + "has been successfully edited.";
+                TempData["Status"] = "success";
+                return RedirectToAction("Index");
+            }
+            return View(acc);
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserAccount u = new WebAppEntities().UserAccounts.Find(id);
+            if (u == null)
+            {
+                return HttpNotFound();
+            }
+            return View(u);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            WebAppEntities db = new WebAppEntities();
+            UserAccount u = db.UserAccounts.Find(id);
+            db.UserAccounts.Remove(u);
+            db.SaveChanges();
+            TempData["Message"] = u.UserName + " " + "has been successfully deleted.";
+            TempData["Status"] = "success";
+            return RedirectToAction("Index");
         }
     }
 }
