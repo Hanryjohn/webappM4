cf3eb496040be9f0fae7fddb7c7d288356e00d14
Mike-Van <miike.vaan@gmail.com>
2018-02-24 08:15:11 +0700

    Initial commit

---
+++ Project1/Controllers/ImagesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private WebAppEntities db = new WebAppEntities();
+
+        // GET: Images
+        public ActionResult Index()
+        {
+            return View(db.Images.ToList());
+        }
+
+        // GET: Images/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            //TempData["imageId"] = id;
+            return View(image); 
+        }
+
+        public ActionResult Show(int id)
+        {
+            var imageData = db.Images.Find(id).img;
+            return File(imageData, "image/jpg");
+        }
+
+        // GET: Images/Create
+        public ActionResult Create()
+        {
+            Image image = new Image();
+            return View(image);
+        }
+
+        // POST: Images/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]        
+        public ActionResult Create(Image image, HttpPostedFileBase file1)
+        {
+            if(file1 != null)
+            {
+                image.img = new byte[file1.ContentLength];
+                file1.InputStream.Read(image.img, 0, file1.ContentLength);
+            }
+            db.Images.Add(image);
+            db.SaveChanges();
+            return View(image);
+        }
+        /*
+        // GET: Images/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // POST: Images/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,img")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(image).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(image);
+        }
+
+        // GET: Images/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // POST: Images/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Image image = db.Images.Find(id);
+            db.Images.Remove(image);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        */
+    }
+}
